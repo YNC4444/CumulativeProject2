3581822d90ae392443f613e709c765d5a6523762
N <N@DESKTOP-5EDURD5>
2024-04-05 22:22:25 -0400

    Add project files.

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,38 @@
+﻿using CumulativeProject2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CumulativeProject2.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher/List
+        public ActionResult List(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
+            return View(Teachers);
+        }
+
+        // GET: Teacher/Show/{id}
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.findTeacher(id);
+
+            return View(NewTeacher);
+        }
+
+        // GET: /Teacher/DeleteConfirm/{id}
+        public ActionResult DeleteConfirm(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.findTeacher(id);
+
+            return View(NewTeacher);
+        }
+    }
+}
---
+++ Controllers/TeacherDataController.cs
@@ -0,0 +1,145 @@
+﻿using CumulativeProject2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace CumulativeProject2.Controllers
+{
+    public class TeacherDataController : ApiController
+    {
+        private SchoolDbContext School = new SchoolDbContext();
+
+        /// <summary>
+        /// Return a list of teachers in the school.
+        /// </summary>
+        /// <example> GET api/TeaacherData/ListTeachers </example>
+        /// <returns>A list of teachers ID, first name, last name, employee number, hire date, and salary. </returns>
+        [HttpGet]
+        [Route("api/TeacherData/ListTeachers/{SearchKey?}")]
+        public IEnumerable<Teacher> ListTeachers(string SearchKey = null)
+        {
+            // Create an instance of a connection
+            MySqlConnection Conn = School.AccessDatabase();
+
+            // Open the connection between the web server & the database
+            Conn.Open();
+
+            // Make a new command/query for the database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            // SQL query
+            cmd.CommandText = "Select * from teachers where lower(teacherfname) like lower(@key) or " +
+                "lower(teacherlname) like lower(@key) or " +
+                "lower(concat(teacherfname, ' ', teacherlname)) like lower(@key) or " +
+                "cast(salary as decimal(10,2)) = @SalaryKey or " +
+                "date(hiredate) = str_to_date(@DateKey, '%Y-%m-%d')";
+
+            // paramaterizing the query for name search
+            cmd.Parameters.AddWithValue("@key", "%" + SearchKey + "%");
+
+            // parameter for salary search
+            decimal SalaryKey;
+            if (Decimal.TryParse(SearchKey, out SalaryKey))
+            {
+                cmd.Parameters.AddWithValue("@SalaryKey", SalaryKey);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@SalaryKey", DBNull.Value);
+            }
+
+            // parameter for hiredate search
+            DateTime DateKey;
+            if (DateTime.TryParse(SearchKey, out DateKey))
+            {
+                cmd.Parameters.AddWithValue("@DateKey", DateKey.ToString("yyyy-MM-dd"));
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@DateKey", DBNull.Value);
+            }
+
+            cmd.Prepare();
+
+            // Gather result set of query into a variable
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            List<Teacher> Teachers = new List<Teacher>();
+
+            while (ResultSet.Read())
+            {
+                // access column information by the database column name as an index
+                int TeacherId = (int)ResultSet["teacherid"];
+                string TeacherFname = (string)ResultSet["teacherfname"];
+                string TeacherLname = (string)ResultSet["teacherlname"];
+                string EmployeeNumber = (string)ResultSet["employeenumber"];
+                DateTime HireDate = (DateTime)ResultSet["hiredate"];
+                decimal Salary = Convert.ToDecimal(ResultSet["salary"]);
+
+                Teacher NewTeacher = new Teacher();
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.EmployeeNumber = EmployeeNumber;
+                NewTeacher.HireDate = HireDate;
+                NewTeacher.Salary = Salary;
+
+                // add author name to the list created above
+                Teachers.Add(NewTeacher);
+            }
+
+            // close the connection between the MySQL database & the web server
+            Conn.Close();
+
+            return Teachers;
+        }
+
+        [HttpGet]
+
+        // used in the Show or List cshtml 
+        public Teacher findTeacher(int id)
+        {
+            Teacher NewTeacher = new Teacher();
+
+            // Create an instance of a connection
+            MySqlConnection Conn = School.AccessDatabase();
+
+            // Open the connection between the web server & the database
+            Conn.Open();
+
+            // Make a new command/query for the database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            // SQL query
+            cmd.CommandText = "Select * from teachers where teacherid =" + id;
+
+            // Gather result set of query into a variable
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            while (ResultSet.Read())
+            {
+                // access column information by the database column name as an index
+                int TeacherId = (int)ResultSet["teacherid"];
+                string TeacherFname = (string)ResultSet["teacherfname"];
+                string TeacherLname = (string)ResultSet["teacherlname"];
+                string EmployeeNumber = (string)ResultSet["employeenumber"];
+                DateTime HireDate = (DateTime)ResultSet["hiredate"];
+                decimal Salary = Convert.ToDecimal(ResultSet["salary"]);
+
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.EmployeeNumber = EmployeeNumber;
+                NewTeacher.HireDate = HireDate;
+                NewTeacher.Salary = Salary;
+            }
+
+            return NewTeacher;
+        }
+    }
+}
